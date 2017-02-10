2db6ac5aadfae7208183079f81b928815e824898
samshal <samueladeshina73@gmail.com>
2017-02-10 18:52:49 +0100

    Added public docs

--- app/public/assets/angular/app.js
+++ app/public/assets/angular/app.js
@@ -5,6 +5,30 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 	.when('/', {
 		templateUrl: 'tpl/index.html'
 	})
+	.when('/what-is-mmm', {
+		templateUrl: 'tpl/docs/whatismmm.html'
+	})
+	.when('/docs/how-it-works', {
+		templateUrl: 'tpl/docs/howmmmworks.html'
+	})
+	.when('/docs/legality', {
+		templateUrl: 'tpl/docs/legality.html'
+	})
+	.when('/docs/privacy-policy', {
+		templateUrl: 'tpl/docs/privacy.html'
+	})
+	.when('/docs/ideology', {
+		templateUrl: 'tpl/docs/ideology.html'
+	})
+	.when('/docs/charity', {
+		templateUrl: 'tpl/docs/charity.html'
+	})
+	.when('/testimonials', {
+		templateUrl: 'tpl/docs/testimonials.html'
+	})
+	.when('/mmm-abroad', {
+		templateUrl: 'tpl/docs/mmmabroad.html'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
